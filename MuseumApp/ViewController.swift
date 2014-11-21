faf6f3036b3996fea7fe3726cff9112d616bfed7
RobinXSI <xsirobin@gmail.com>
2014-11-21 18:13:12 +0100

    Initial Commit

---
+++ MuseumApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MuseumApp
+//
+//  Created by Robin on 21.11.14.
+//  Copyright (c) 2014 HSR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
